4fbbf846d47692dbfc559f990fabfeb068e65192
jacksakers <46449079+jacksakers@users.noreply.github.com>
2021-11-12 00:57:36 -0500

    Update lane_chaser.py

--- packages/project3/src/lane_chaser.py
+++ packages/project3/src/lane_chaser.py
@@ -2,43 +2,26 @@
 
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
-from duckietown_msgs.msg import Pose2DStamped
+from duckietown_msgs.msg import LanePose
 from time import sleep
 
-class PIDCalc:
-    def __init__(self, P=2.0, I=0.0, D=1.0, Derivator=0, Integrator=0):
-        self.kP = P
-        self.Ki = I
-        self.Kd = D
-        self.Derivator = Derivator
-        self.integral = 0
-        self.dt = .1;
-        self.set_point = 0.0
-        self.error = 0.0
-    def update(self.measured_value):
-        self.error = self.set_point - self.measured_value
-        integral = self.integral + self.error + self.dt
-        self.Derivative = (self.error - self.previous_error) / self.dt
-        self.previous_error = self.error
-        PID = self.kP * self.error + self.ki * self.integral + self.Kd * self.Derivative
-        return PID
-
-class talker:
-    def _init_(self):
-        sub = rospy.Subscriber('lane_controller_node/lane_post', Pose2DStamped, self.talk)
-        pub = rospy.Publisher('car_cmd_switch_node/cmd', Twist2DStamped, queue_size=10)
-        rospy.init_node('lane_chaser', anonymous=True)
-        rate = rospy.Rate(10)
-    def talk(self, data):
-        __init__()
-        vel, o = update(data.data)
-        msg = Twist2DStamped(header=None, v=vel, omega=o)
-        pub.publish(msg)
-if __name__ == '__main__':
-    try:
-        _init_(self)
+class LaneChaser:
+    def __init__(self):
+        self.pub = rospy.Publisher('/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=10)
+        rospy.Subscriber('/lane_filter_node/lane_pose', LanePose, self.listener)
+        
+    def listener(self, data):
+        self.input = data.data
+        rospy.loginfo("JACK SAKERS LANE FOLLOWER IS WORKING!")
+        
+    def talker(self):
+        msg = Twist2DStamped(header=None, v=1, omega=0)
         while not rospy.is_shutdown():
-            talk()
-            rospy.logerror("Jack Sakers Lane Following Node")
-    except rospy.ROSInterruptException:
-        pass
+            if hasattr(self, 'input'):
+                self.pub.publish(msg)
+            sleep(0.5)
+
+
+if __name__ == '__main__':
+    rospy.init_node('lane_chaser')
+    Homework5()
