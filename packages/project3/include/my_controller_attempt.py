f31441770c443d0e6bca705c38f7ff5a092eaa84
jacksakers <46449079+jacksakers@users.noreply.github.com>
2021-11-12 17:35:03 -0500

    Update my_controller_attempt.py

--- packages/project3/include/my_controller_attempt.py
+++ packages/project3/include/my_controller_attempt.py
@@ -3,41 +3,28 @@ import numpy as np
 
 class LaneController:
 
-    This implementation is a simple PD controller.
-
-    Args:
-        ~v_bar (:obj:`float`): Nominal velocity in m/s
-        ~k_d (:obj:`float`): Proportional term for lateral deviation
-        ~k_theta (:obj:`float`): Proportional term for heading deviation
-        ~d_thres (:obj:`float`): Maximum value for lateral error
-        ~theta_thres (:obj:`float`): Maximum value for heading error
-        ~d_offset (:obj:`float`): Goal offset from center of the lane
-        ~integral_bounds (:obj:`dict`): Bounds for integral term
-        ~d_resolution (:obj:`float`): Resolution of lateral position estimate
-        ~phi_resolution (:obj:`float`): Resolution of heading estimate
-        ~omega_ff (:obj:`float`): Feedforward part of controller
-        ~verbose (:obj:`bool`): Verbosity level (0,1,2)
-
-    """
+    #This is my attempt at a PD controller.
 
     def __init__(self, parameters):
         self.parameters = parameters
-        self.d_I = 0.0
-        self.phi_I = 0.0
-        self.prev_d_err = 0.0
-        self.prev_phi_err = 0.0
+        self.prev_d_err = 1.0
+        self.prev_phi_err = 1.0
 
     def update_parameters(self, parameters):
         self.parameters = parameters
 
     def compute_control_action(self, d_err, phi_err, dt, wheels_cmd_exec):
 
-
         self.reset_if_needed(d_err, phi_err, wheels_cmd_exec)
 
+        self.d_deriv = np.abs(d_err - self.prev_d_err) / dt
+        self.phi_deriv = np.abs(phi_err - self.prev_phi_err) / dt
+        
         omega = (
             self.parameters["~k_d"].value * d_err
             + self.parameters["~k_theta"].value * phi_err
+            + self.d_deriv * d_err
+            + self.phi_deriv * phi_err
         )
 
         self.prev_d_err = d_err
