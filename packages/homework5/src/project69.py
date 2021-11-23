247d80b23eb7c52c2a143c40bb4f9edec2392298
jacksakers <46449079+jacksakers@users.noreply.github.com>
2021-11-22 19:08:12 -0500

    Update project69.py

--- packages/homework5/src/project69.py
+++ packages/homework5/src/project69.py
@@ -3,10 +3,18 @@
 import rospy
 from std_msgs.msg import Float32
 from time import sleep
+from duckietown_msgs.msg import (
+    Twist2DStamped,
+    LanePose,
+    WheelsCmdStamped,
+    BoolStamped,
+    FSMState,
+    StopLineReading,
+)
 
 class Project69:
     def __init__(self):
-        self.pub = rospy.Publisher('/duck32/car_cmd/cmd', Float32, queue_size=1) #CHANGE TO CORRECT TOPIC and type
+        self.pub = rospy.Publisher('/duck32/wheels_driver_node/wheels_cmd', WheelsCmdStamped, queue_size=1) #CHANGE TO CORRECT TOPIC and type
         self.v_left = 0.0
         self.v_right = 0.0
         self.firstpause = rospy.get_param('firstpause', None)
@@ -22,7 +30,7 @@ class Project69:
           sleep(self.secondpause)
 
     def talker(self):
-        msg = Twist2DStamped(self.v_left, self.v_right) #change to correct type
+        msg = WheelsCmdStamped(None, self.v_left, self.v_right) #change to correct type
         while not rospy.is_shutdown():
           self.pub.publish(msg)
 
